9f62be6b39499d0ec4a1b1ea74c0c1ef573da0f3
ballibora <110371347+ballibora@users.noreply.github.com>
2022-08-07 15:59:18 +0300

    Update tweet_1_takipc.py

--- tweet_1_takipc.py
+++ tweet_1_takipc.py
@@ -1,8 +1,9 @@
 import tweepy
-TWITTER_CONSUMER_KEY = 'ZPw9IUsuKUFomxA2ZcVhl9FDD'
-TWITTER_CONSUMER_SECRET = 'z46ky2QJzxGOBYVNK7ShqhQewmTEWCFabmLGnReqoxYIbWxgGj'
-TWITTER_ACCESS_TOKEN = '83256274-GaBHrEwopd31LYJ5cgtt6XFFvRJNQcafnKfPOXGQB'
-TWITTER_ACCESS_TOKEN_SECRET = 'UrldiAOGCBnLnlfZ8rbnbOIOj46ldXifau06NiXb4g2mb'
+#boş bırakılan yerler kişiye özel olarak tanımlanır
+TWITTER_CONSUMER_KEY = ''
+TWITTER_CONSUMER_SECRET = ''
+TWITTER_ACCESS_TOKEN = ''
+TWITTER_ACCESS_TOKEN_SECRET = ''
 auth = tweepy.OAuthHandler(TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET)
 auth.set_access_token(TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth,wait_on_rate_limit=True)
@@ -19,7 +20,7 @@ for takipci in takipciler:
 
 #yöntem2
 i=0
-takipciler = tweepy.Cursor(api.followers, id="turkayyildiz965").items(100)
+takipciler = tweepy.Cursor(api.followers, id="").items(100) #idye takipçilerini öğrenmek istediğiniz kullanıcının kullanıcı adını girmeniz gerekir
 for takipci in takipciler:
     i=i+1
     print(i,takipci.name,takipci.screen_name)
